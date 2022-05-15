64c1e2287c0599d5dc5af6de8006c0874b487174
PauloDev13 <devpgm2020@gmail.com>
2022-05-15 18:36:10 -0300

    Mapping Users

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Response} from "../interface/response";
+import {User} from "../interface/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private readonly apiUrl: string = 'https://randomuser.me/api/';
+
+  constructor( private http: HttpClient) { }
+
+  getUsers(size: number = 10): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/?results=${size}`);
+  }
+
+  getUser(uuid: number = 1): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/?uuid=${uuid}`);
+  }
+
+  private processResponse(response: Response): Response {
+    return {
+      info: {...response.info},
+      results: response.results.map((user: any) => (<User>{
+        uuid: user.login.uuid,
+        firstName: user.name.first,
+        lastName: user.name.last,
+        email: user.email,
+        username: user.login.username,
+        gender: user.gender,
+        address: `${user.location.street.number}
+                  ${user.location.street.name}
+                  ${user.location.city},
+                  ${user.location.country}`,
+        dataOfBirth: user.dob.date,
+        phone: user.phone,
+        imageUrl: user.picture.medium,
+        coordinate: {
+          latitude: user.location.coordinates.latitude,
+          longitude: user.location.coordinates.longitude
+        }
+      }))
+    };
+  }
+
+}
